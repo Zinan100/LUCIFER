39d27ea7f48bdd452e24e8a59a3a852867903f21
Zinan100 <97530806+Zinan100@users.noreply.github.com>
2022-01-27 11:55:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,7 +18,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('🤖 Support', url='https://t.me/Ls_Supportz')
+                InlineKeyboardButton('🤖 Support', url='https://t.me/+uTA_ibAsyWU4OGE1')
             ],
             [
                 InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
@@ -40,10 +40,13 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Support', url='https://t.me/Ls_Supportz')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('🔊CHANNEL🔊', url='https://t.me/+uTA_ibAsyWU4OGE1')
+            ],[
+            InlineKeyboardButton('💬GROUP💬', url='https://t.me/+uTA_ibAsyWU4OGE1')
+            ],[
+            InlineKeyboardButton('🆘 Help 🆘', callback_data='help'),
+            InlineKeyboardButton('🤠 About 🤠', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -81,10 +84,13 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Support', url='https://t.me/Ls_Supportz')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('🔊CHANNEL🔊', url='https://t.me/+uTA_ibAsyWU4OGE1')
+            ],[
+            InlineKeyboardButton('💬GROUP💬', url='https://t.me/+uTA_ibAsyWU4OGE1')
+            ],[
+            InlineKeyboardButton('🆘 Help 🆘', callback_data='help'),
+            InlineKeyboardButton('🤠 About 🤠', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
